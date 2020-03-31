cbb1b7c4dab7d1a8cafdf517382b7b3071f0eb80
Ruan Bekker <ruan.ru.bekker@gmail.com>
2020-03-31 23:34:22 +0200

    verfy all configs

--- 0-scripts/install_pushgateway.sh
+++ 0-scripts/install_pushgateway.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
 PUSHGATEWAY_VERSION="1.2.0"
-
+# useradd --no-create-home --shell /bin/false prometheus
 wget https://github.com/prometheus/pushgateway/releases/download/v${PUSHGATEWAY_VERSION}/pushgateway-${PUSHGATEWAY_VERSION}.linux-amd64.tar.gz
 tar -xf pushgateway-${PUSHGATEWAY_VERSION}.linux-amd64.tar.gz
 cd pushgateway-${PUSHGATEWAY_VERSION}.linux-amd64/
 mv pushgateway /usr/local/bin/
+#mkdir var/pushgateway
+#chown -R prometheus:prometheus /var/pushgateway
 systemctl daemon-reload
 systemctl restart pushgateway
-
 # test
 # echo "cpu_utilization 20.25" | curl --data-binary @- http://localhost:9091/metrics/job/my_custom_metrics/instance/10.20.0.1:9000/provider/hetzner
